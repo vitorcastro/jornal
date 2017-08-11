5b362c4f319ed52a3760da38a007b8fb15fd9772
Vitor Castro <castro.vitor006@gmail.com>
2017-08-10 21:57:13 -0300

    Adicionando chave primária auto incremental na notícia

--- src/br/unifesspa/model/Noticia.java
+++ src/br/unifesspa/model/Noticia.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -15,7 +16,7 @@ import javax.persistence.NamedQuery;
 public class Noticia {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String titulo;
 	private String descricao;
